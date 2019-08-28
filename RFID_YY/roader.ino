39f6f6244c1e9d279e8e60d119af55e39985b75d
Alexander Semenovich <53298672+SemenovichRobatzNetwork@users.noreply.github.com>
2019-08-28 12:21:28 +0300

    Add files via upload

---
+++ RFID_YY/roader.ino
@@ -0,0 +1,282 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <ThreeWire.h>
+#include <RtcDS1302.h>
+#include "leds.h"
+#include <esp32-hal-bt.h>
+#include <esp_bt.h>
+#include <EEPROM.h>
+
+#define RST_PIN     22
+#define SS_PIN      21
+#define VOLTAGE_PIN 33
+
+#define RTC_RST     26
+#define RTC_CLK     14
+#define RTC_DATA    27
+
+#define LED_RED     16
+#define LED_GREEN   17
+#define LED_BLUE    5
+
+#define BUTTON_PIN  32
+
+#define WIFI_RETRIES 30
+#define WIFI_SSID "Room89"
+#define WIFI_PASS "room8989"
+#define AUTO_FLUSH_INTERVAL (35000UL)
+
+#define uS_TO_S_FACTOR 1000000  //Conversion factor for micro seconds to seconds
+#define TIME_TO_SLEEP  30        //Time ESP32 will go to sleep (in seconds)
+
+
+int analogInput = 33 ;
+float vin = 0.0;
+float k_divider = 4.83; // resistor divider 47k 12k
+float v_ref_ADC = 1.1;
+int value = 0;
+int startAddr = 0;
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+ThreeWire myWire(RTC_DATA, RTC_CLK, RTC_RST); // IO, SCLK, CE
+
+RtcDS1302<ThreeWire> Rtc(myWire);
+
+struct RfidScanEvent {
+  byte uid[4];
+  long unsigned int time;
+  int voltage;
+  byte checkSum;
+};
+
+struct RfidScanEvent rfidScanEvents[200];
+
+int nextRfidScanEventIdx = 0;
+unsigned long lastFlushAttemptAt = 0;
+
+void checkRFIDScanner(void *parameter);
+void flushData(void *parameter);
+String formatRfidScanEvent(const RfidScanEvent &wpt);
+String formatBatteryVoltage(const uint16_t voltage);
+uint16_t getBatteryVoltage();
+
+void checkRfidScannerTask(void *parameter);
+void flushDataTask(void *parameter);
+void sleepStart();
+
+void setup()
+{
+  SPI.begin();
+  Serial.begin(115200);
+
+  mfrc522.PCD_Init();
+  mfrc522.PCD_DumpVersionToSerial();
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  Serial.print("compiled: ");
+  Serial.print(__DATE__);
+  Serial.println(__TIME__);
+  
+  Rtc.Begin();
+  
+  EEPROM.begin(128);
+  
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  Rtc.SetDateTime(compiled);
+  
+  WiFi.mode(WIFI_OFF);
+  
+  btStop();
+  
+  setupLed(LED_RED);
+  setupLed(LED_GREEN);
+  setupLed(LED_BLUE);
+  
+  pinMode(VOLTAGE_PIN, INPUT);
+  
+  rfidScanEvents[nextRfidScanEventIdx].voltage = analogRead(VOLTAGE_PIN);
+  
+  xTaskCreate(&flushDataTask, "flushDataTask", 8192, NULL, 1, NULL);
+  xTaskCreate(&checkRfidScannerTask, "checkRfidScannerTask", 8192, NULL, 5, NULL);
+  
+  delay(3000);
+  
+  sleepStart();
+}
+
+void loop()
+{
+  //infinite loop
+}
+
+void checkRfidScannerTask(void *parameter)
+{
+  for (;;)
+  {
+    checkRfidScanner();
+    vTaskDelay(100);
+  }
+  vTaskDelete(NULL);
+}
+
+void flushDataTask(void *parameter)
+{
+  for (;;)
+  {
+    if (lastFlushAttemptAt == 0 || millis() - lastFlushAttemptAt >= AUTO_FLUSH_INTERVAL)
+    {
+      flushData();
+
+    }
+    vTaskDelay(1000);
+  }
+  vTaskDelete(NULL);
+}
+
+void checkRfidScanner()
+{
+  for (;;)
+  {
+    if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial())
+    {
+      return;
+    }
+
+    String uidString;
+    uidString = "";
+    for (int i = 0; i < 4; i++)
+    {
+      rfidScanEvents[nextRfidScanEventIdx].uid[i] = mfrc522.uid.uidByte[i];
+      uidString += String(rfidScanEvents[nextRfidScanEventIdx].uid[i]);
+      Serial.println(rfidScanEvents[nextRfidScanEventIdx].uid[i]);
+    }
+      Serial.println(uidString + " and " + (String(rfidScanEvents[nextRfidScanEventIdx - 1].uid[0]) + String(rfidScanEvents[nextRfidScanEventIdx - 1].uid[1]) + String(rfidScanEvents[nextRfidScanEventIdx - 1].uid[2]) + String(rfidScanEvents[nextRfidScanEventIdx - 1].uid[2])));
+    
+    blinkLed(LED_GREEN, 500);
+    
+    if (nextRfidScanEventIdx == 0 || (nextRfidScanEventIdx > 0 && (String(rfidScanEvents[nextRfidScanEventIdx - 1].uid[0]) + String(rfidScanEvents[nextRfidScanEventIdx - 1].uid[1]) + String(rfidScanEvents[nextRfidScanEventIdx - 1].uid[2]) + String(rfidScanEvents[nextRfidScanEventIdx - 1].uid[2]))  != uidString))
+    {
+      Serial.println("RtcTime");
+      
+      RtcDateTime now = Rtc.GetDateTime();
+      
+      rfidScanEvents[nextRfidScanEventIdx].time = now.Epoch32Time();
+      rfidScanEvents[nextRfidScanEventIdx].checkSum = sizeof(rfidScanEvents[nextRfidScanEventIdx].uid) + sizeof(rfidScanEvents[nextRfidScanEventIdx].voltage) + sizeof(rfidScanEvents[nextRfidScanEventIdx].time);
+      Serial.println("EEPROM COMMIT");
+      EEPROM.put(startAddr, rfidScanEvents[nextRfidScanEventIdx]);
+      EEPROM.commit();
+      Serial.println(" EEPROM COMMIT + TEST END ");
+      nextRfidScanEventIdx++;
+    }
+
+    if (nextRfidScanEventIdx > 0)
+    {
+      flushData();
+    }
+  }
+}
+
+bool connectWifi()
+{
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    return true;
+  }
+
+  int timeout = 2000;
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(250);
+    timeout -= 250;
+    if (timeout <= 0)
+    {
+      WiFi.disconnect();
+      delay(500);
+      WiFi.mode(WIFI_OFF);
+      return false;
+    }
+  }
+  return true;
+}
+
+void flushData()
+{
+  onLed(LED_BLUE);
+  lastFlushAttemptAt = millis();
+
+  if (!connectWifi())
+  {
+    blinkLed(LED_BLUE, 500);
+    blinkLed(LED_RED, 250, 3);
+    return;
+  }
+
+  int httpResponseCode;
+  HTTPClient http;
+  http.begin("http://roader.herokuapp.com/api/devices/flush");
+  http.addHeader("Content-Type", "text/plain");
+  
+  String data;
+  
+  RfidScanEvent MessageEvent; 
+  EEPROM.get(startAddr, MessageEvent);
+  
+  Serial.println(MessageEvent.uid[0]);
+  Serial.println(MessageEvent.time);
+  
+  String Msg = String(MessageEvent.uid[0]) + String(MessageEvent.uid[1]) + String(MessageEvent.uid[2]) + String(MessageEvent.uid[3]) + "   " + String(MessageEvent.time) + "   " + String(MessageEvent.voltage) + "    " + String(MessageEvent.checkSum);
+  
+  Serial.println("GREK ZDES TEST IDET: ");
+  Serial.println(Msg);
+  httpResponseCode = http.POST(Msg);
+  http.end();
+  WiFi.disconnect();
+  delay(500);
+  
+  WiFi.mode(WIFI_OFF);
+  
+  delay(2000);
+  offLed(LED_BLUE);
+  RtcDateTime now = Rtc.GetDateTime();
+  
+  Serial.println(httpResponseCode);
+  
+  if (httpResponseCode == 200)
+  {
+    nextRfidScanEventIdx = 0;
+    blinkLed(LED_GREEN, 250, 3);
+  }
+  else
+  {
+    blinkLed(LED_RED, 250, 3);
+  }
+  sleepStart();
+} 
+
+void sleepStart()
+{
+  Serial.println(analogRead(BUTTON_PIN));
+  delay(666);
+  Serial.println("Sleep Time");
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_32, 1);
+  esp_light_sleep_start();
+  esp_sleep_wakeup_cause_t wakeup_reason;
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+  switch (wakeup_reason)
+  {
+    case 1  : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case 2  : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case 3  : Serial.println("Wakeup caused by timer"); break;
+    case 4  : Serial.println("Wakeup caused by touchpad"); break;
+    case 5  : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.println("Wakeup was not caused by deep sleep"); break;
+  }
+}
